76e1ff9557dbc9a4833c05b06fb7ffd69f72d8e9
dkzcm <xmk.hg28@gmail.com>
2024-09-22 21:56:26 +0800

    add button storiy-book-test

--- packages/play/src/stories/Button.stories.ts
+++ packages/play/src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type {Meta, StoryObj, ArgTypes} from '@storybook/vue3'
 import {fn, within, userEvent, expect, clearAllMocks} from '@storybook/test'
-
+import {set} from 'lodash-es'
 import {XmkButton} from 'xmk-ui'
 
 type Story = StoryObj<typeof XmkButton> & {argTypes?: ArgTypes}
@@ -77,35 +77,39 @@ export const Default: Story & {args: {content: string}} = {
 
   play: async ({canvasElement, args, step}) => {
     const canvas = within(canvasElement)
-    const btn = canvas.getByTestId('stort-test-btn')
+    const btn = canvas.getByTestId('story-test-btn')
 
-    // await step('when useThrottle is set to true,the onClick should be called once', async () => {
-    //   set(args, 'useThrottle', true)
-    //   await userEvent.tripleClick(btn)
-    //   expect(args.onClick).toHaveBeenCalledTimes(1)
-    //   clearAllMocks()
-    // })
+    await step('when useThrottle is set to true,the onClick should be called once', async () => {
+      set(args, 'useThrottle', true)
+      await userEvent.tripleClick(btn)
+      expect(args.onClick).toHaveBeenCalledTimes(1)
+      clearAllMocks()
+    })
 
-    // await step('when useTrhottle is set to false,the onClick should be called three times', async () => {
-    //   set(args, 'useThrottle', false)
-    //   await userEvent.tripleClick(btn)
-    //   expect(args.onClick).toHaveBeenCalledTimes(3)
-    //   clearAllMocks()
-    // })
+    await step('when useTrhottle is set to false,the onClick should be called three times', async () => {
+      set(args, 'useThrottle', false)
+      await userEvent.tripleClick(btn)
+      expect(args.onClick).toHaveBeenCalledTimes(3)
+      clearAllMocks()
+    })
 
-    // await step('when disabled is set to true, the onClick should not be called', async () => {
-    //   set(args, 'disabled', true)
-    //   await userEvent.click(btn)
+    await step('when disabled is set to true, the onClick should not be called', async () => {
+      set(args, 'disabled', true)
+      await userEvent.click(btn)
 
-    //   expect(args.onClick).not.toHaveBeenCalled()
-    //   set(args, 'disabled', false)
-    //   clearAllMocks()
-    // })
+      expect(args.onClick).not.toHaveBeenCalled()
+      set(args, 'disabled', false)
+      clearAllMocks()
+    })
 
-    // await step('when loading is set to true, the button should be disabled', async () => {
-    //   set(args, 'loading', true)
-    //   expect(btn).toHaveAttribute('disabled')
-    // })
+    await step('when loading is set to true, the button should be disabled', async () => {
+      set(args, 'loading', true)
+      await userEvent.click(btn)
+
+      expect(args.onClick).toHaveBeenCalledTimes(0)
+      set(args, 'loading', false)
+      clearAllMocks()
+    })
   }
 }
 
